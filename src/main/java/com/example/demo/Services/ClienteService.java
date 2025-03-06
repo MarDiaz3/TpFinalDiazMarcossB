c74800395883cfb706ebf37a15e5a00dd3144f36
MarDiaz3 <bendiaz2908@gmail.com>
2025-03-06 17:04:52 -0300

    creamos comandos en Service Cliente y Producto

--- src/main/java/com/example/demo/Services/ClienteService.java
+++ src/main/java/com/example/demo/Services/ClienteService.java
@@ -1,6 +1,82 @@
 package com.example.demo.Services;
 
+import com.example.demo.Models.Cliente;
+import com.example.demo.Models.Producto;
+import com.example.demo.Repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+
+@Service
 
 public class ClienteService {
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+
+    //obtiene tddos los products verdaderos (est=true)+
+    public List<Cliente> traeCliente() { //trae todos los pruduct de la BD
+    return clienteRepository.findByEstadoTrue();
+    }
+
+
+    public Cliente traeClientePorId (Integer id) {
+        if (id == null) {
+            throw new IllegalArgumentException("El ID no puede ser nulo");//trae un product por ID(busca el id)
+        }
+        return clienteRepository.findById(id).orElse(null);
+    }
+
+
+       //crea un nuevo cliente
+    public void crearCliente(Cliente cliente){
+    if (cliente == null) {
+        throw new IllegalArgumentException ("El cliente no puede ser nulo, no puede estar vacio");
+    }
+    clienteRepository.save (cliente);
+    }
+
+
+    //actualizar cliente
+    public void actualizarCliente(Cliente cliente) {
+        if (cliente == null || cliente.getId () == null){
+            throw new IllegalArgumentException (" el cliente o id no puede ser null");
+        }
+        if (!clienteRepository.existsById(cliente.getId())) {
+            throw new IllegalArgumentException("producto con id " + cliente.getId() + "no encontrado");
+        }
+        clienteRepository.save(cliente);
+    }
+
+
     
+    //eliminar cliente (actualizar el estado a false o esconder)
+    public Cliente eliminarCliente (Integer id) {
+        if(id == null) {
+            throw new IllegalArgumentException("El id no puede ser null");
+        }
+        Cliente cliente = clienteRepository.findById(id).orElse(null);
+        if (cliente != null) {
+            cliente.setEstado(false);
+            return clienteRepository.save(cliente);
+        }
+        return null;
+
+    }
+    
+
+
+
+
+
+
+
+
+
+
 }
+
+
+
+
--- src/main/java/com/example/demo/Services/ProductoService.java
+++ src/main/java/com/example/demo/Services/ProductoService.java
@@ -26,9 +26,46 @@ public class ProductoService {
         return productoRepository.findById(id).orElse(null);
     }
 
+    //crea un nuevo producto
+    public void crearProducto (Producto producto){
+    if (producto == null) {
+        throw new IllegalArgumentException ("El producto no puede ser nulo, no puede estar vacio");
+    }
+    productoRepository.save (producto);
+    }
+    
+    //actualizar producto
+    public void actualizarProducto(Producto producto) {
+        if (producto == null || producto.getId () == null){
+            throw new IllegalArgumentException (" el producto o id no puede ser null");
+        }
+        if (!productoRepository.existsById(producto.getId())) {
+            throw new IllegalArgumentException("producto con id " + producto.getId() + "no encontrado");
+        }
+        productoRepository.save(producto);
+    }
+
+
+    //eliminar producto(actualizar el estado a false o esconder)
+    public Producto eliminarProducto (Integer id) {
+        if(id == null) {
+            throw new IllegalArgumentException("El id no puede ser null");
+        }
+        Producto producto = productoRepository.findById(id).orElse(null);
+        if (producto != null) {
+            producto.setEstado(false);
+            return productoRepository.save(producto);
+        }
+        return null;
+
+    }
+    
+
+
+    }
 
 
 
 
 
-}
+
